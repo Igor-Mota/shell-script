8ff98a5b4d57b3caf1a47a2687be5bb906b91e12
igor-mota <igormota999@gmail.com>
2022-10-25 20:46:24 -0300

    add more

---
+++ if-numerico.sh
@@ -0,0 +1,22 @@
+# !/bin/bash
+
+# para usar comparaçoes numericas deve se usar o 
+# comando test ele atribui o valor do ultimo status 
+# de comando para 0 no caso de true e para 1 no caso de false 
+
+if test 1 -eq 1
+  then
+    echo "1 e igual a 1"
+fi
+
+# A tabela de copara tivos e diferente das linguagens de programação tradicionais
+
+# -eq | == | igual
+# -ge | >= | maior ou igual
+# -gt | >  | maior
+# -le | <= | menor ou igual
+# -lt | <  | menor
+# -ne | != | diferente
+
+
+
---
+++ if-string.sh
@@ -0,0 +1,44 @@
+# !/bin/bash
+
+# os comparadores de strings sao diferentes dos comparadores numericos
+
+#  = | igual
+# != | diferente
+#  < | menor
+#  > | maior
+# -n | se a largura e maior que zero
+# -z | se e igual a zero
+
+
+if test "carro" = "carro"
+  then
+    echo "carro e igual a carro"
+fi
+
+if test "carro" != "cadeira"
+  then 
+    echo "carro e dirente de cadeira"
+fi
+
+if test "O rato roeu a roupa do rei de roma" > "O rato roeu"
+then
+  echo "O rato roeu a roupa do rei de roma e maior que O rato roeu"
+fi
+
+if test "O rato roeu a roupa do rei de roma" < "O rato roeu"
+then
+  echo "O rato roeu e menor que  O rato roeu a roupa do rei de roma"
+fi
+
+if test -n "usuario"
+  then
+    echo "a string usuario nao vazia"
+fi
+
+if test -z ""
+  then
+    echo "A string esta vazia"
+fi
+
+
+
